03d303c589865a8c358ed8daab4a3150e57786ed
Liliia <LiliiaPav@gmail.com>
2024-10-29 16:28:11 -0700

    .click and .off

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function sayHello(){
-    $('#title').html('Goodbye')
+// function sayHello(){
+//     if($('#title').html()=='Hello'){
+//         $('#title').html('Goodbye')
+//     }
+//     else {
+//         $('#title').html('Hello')
+//     }
+
+    
+// }
+
+
+function sayBye(){
+    $('#title').html('Goodbye');
+    $('#title').click(function(){
+        $('#title').html('Hello');
+        $('#title').off('click');
+    });
+    console.log( $('#title').html());
+    
 }
